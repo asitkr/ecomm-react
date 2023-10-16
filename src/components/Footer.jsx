e8fd06d0dc92b9eb90c2ebc0c8fa73fb8808dba2
Ashit Kumar Sinha <37330958+asitkr@users.noreply.github.com>
2023-10-16 21:18:47 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -24,7 +24,7 @@ const Footer = () => {
         <footer>
             <div className='container grid grid-four-column'>
                 <div className='footer-about'>
-                    <h3>Thapa Technical</h3>
+                    <h3>Ecom</h3>
                     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. </p>
                 </div>
                 <div className='footer-subscribe'>
